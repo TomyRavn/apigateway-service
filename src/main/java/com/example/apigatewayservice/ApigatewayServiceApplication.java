65d48d4b224fa6a448367bf93b7ccfc894dd731c
TomyRavn <godqwer123@naver.com>
2023-03-17 16:31:52 +0900

    spring-boot actuator 추가 및 Profiles 설정

--- src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,13 +1,21 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public HttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
